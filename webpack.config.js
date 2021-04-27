fd40d4bd7f32ae9d80a0fdc16e07f7c5be9d0f68
kage1414 <kylejohnson92294@gmail.com>
2021-04-27 09:42:46 -0400

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,22 @@
 const path = require('path');
-import html from './file.html';
+const html = './file.html';
 var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
-    filename: `bundle-${Date.now()}.js`,
+    filename: 'bundle.js',
     path: DIST_DIR
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
         }
-      },
-      {
-        test: /\.(jpe?g|gif|png|svg|woff|ttf|wav|html|mp3)$/,
-        loader: 'file-loader'
-      },
-      {
-        test: /\.html$/i,
-        loader: 'html-loader'
       }
     ]
   }
