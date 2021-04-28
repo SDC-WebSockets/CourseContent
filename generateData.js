2d9835ae672db9a14b52b2860b7b39c489d34fbb
kage1414 <kylejohnson92294@gmail.com>
2021-04-28 19:25:47 -0400

    Move to mobile

--- generateData.js
+++ generateData.js
@@ -11,6 +11,7 @@ const dbName = process.env.dbName || config.dbName;
 const fs = require('fs');
 const path = require('path');
 const {checkIntegrity} = require('untegrity');
+const AWS = require('aws-sdk');
 
 mongoose.connect(dbUrl, {dbName: dbName});
 
@@ -443,4 +444,65 @@ const checkAll = () => {
   });
 };
 
-checkAll();
+// checkAll();
+
+const awsId = config.accessKeyID;
+const awsSecret = config.secretAccessKey;
+
+const BUCKET_NAME = 'charlotte-badger-course-content-stock-footage';
+// const BUCKET_NAME = 'test-bucket-lksjdfhgsldkfjgsdlfkjgndsflkgjnsd';
+
+const s3 = new AWS.S3({
+  accessKeyId: awsId,
+  secretAccessKey: awsSecret
+});
+
+const params = {
+  Bucket: BUCKET_NAME,
+  CreateBucketConfiguration: {
+    LocationConstraint: 'eu-west-2'
+  }
+};
+
+// s3.createBucket(params, function(err, data) {
+//   if (err) console.log(err, err.stack);
+//   else console.log('Bucket Created Successfully', data.Location);
+// });
+
+
+
+const uploadOneFile = (fileName) => {
+  // Read content from the file
+  const fileContent = fs.readFileSync(fileName);
+
+  // Setting up S3 upload parameters
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: fileName, // File name you want to save as in S3
+    Body: fileContent,
+    ContentType: 'video/mp4'
+  };
+
+  // Uploading files to the bucket
+  s3.upload(params, function(err, data) {
+    if (err) {
+      throw err;
+    }
+    console.log(`File uploaded successfully. ${data.Location}`);
+  });
+};
+
+const uploadDirectory = (directory) => {
+  console.log('files not read')
+  console.log(directory);
+  let files = fs.readdirSync(directory);
+    console.log('files read')
+    for (let i = 0; i < files.length; i++) {
+      const filePath = path.join(directory, files[i]);
+      uploadOneFile(filePath);
+    }
+
+
+};
+
+uploadDirectory(path.join(__dirname, 'videos'));
