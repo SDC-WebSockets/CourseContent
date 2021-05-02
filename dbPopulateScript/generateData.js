e9a95d4e26a6fb584e9f13c855eb2382ffe40c2c
Kyle Johnson <kylejohnson92294@gmail.com>
2021-05-02 16:36:38 -0400

    Delete dbPopulateScript directory

--- dbPopulateScript/generateData.js
+++
@@ -1,7 +0,0 @@
-// Generate Database Model
-const axios = require('axios');
-const moment = require('moment');
-const fs = require('fs');
-const path = require('path');
-const {checkIntegrity} = require('untegrity');
-const AWS = require('aws-sdk');
