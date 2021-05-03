c35d7fb35622412a123a3a1caf3f2533a49bf2cc
kage1414 <kylejohnson92294@gmail.com>
2021-05-03 13:58:04 -0400

    Add ContentHeader

---
+++ client/src/components/ContentHeader.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import moment from 'moment';
+
+const ContentHeader = (props) => (
+
+  <div style={{ maxWidth: '600px', maxHeight: '40px' }}>
+    <div style={{ maxWidth: '310px', maxHeigth: '35px', float: 'left' }}>
+      <span>
+        {props.totalSections} Sections • {props.totalLectures + props.totalArticles} Lectures • <span><span>{moment(props.totalLength).format('H[h ]M[m]')}</span> total length</span>
+      </span>
+    </div>
+    <button style={{ float: 'right' }} type="button">
+      <span>
+        Expand all Sections
+      </span>
+    </button>
+  </div>
+
+);
+
+export default ContentHeader;
--- client/src/components/CourseContent.jsx
+++ client/src/components/CourseContent.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import axios from 'axios';
 import Section from './Section.jsx';
 import moment from 'moment';
+import ContentHeader from './ContentHeader.jsx';
 
 const CourseContent = (props) => {
 
   console.log(props.course);
   return (
     <div>
-      <div style={{ maxWidth: '600px', maxHeight: '40px' }}><div style={{ maxWidth: '310px', maxHeigth: '35px' }}><span>{props.course.totalSections} Sections • {props.course.totalLectures + props.course.totalArticles} Lectures • <span><span>{moment(props.course.totalLength).format('H[h ]M[m]')}</span> total length</span></span></div></div>
-      {props.course.sections.length > 0 &&
-        props.course.sections.map(section => {
-          console.log(section.sectionId);
-          return <Section key={section.sectionId} section={section} />;
-        })}
+      <ContentHeader totalSections={props.course.totalSections} totalLectures={props.course.totalLectures} totalArticles={props.course.totalArticles} totalLength={props.course.totalLength}/>
+      <br/>
+      <div>
+        {props.course.sections.length > 0 &&
+          props.course.sections.map(section => {
+            console.log(section.sectionId);
+            return <Section key={section.sectionId} section={section} />;
+          })}
+      </div>
     </div>
   );
 
