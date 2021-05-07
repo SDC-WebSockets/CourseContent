d90892f1c27c2b07db90b0203c02777b5f413fb9
kage1414 <kylejohnson92294@gmail.com>
2021-05-06 23:01:14 -0400

    Write three successful tests. Working on passing one

--- test/__tests__/CourseContent.test.js
+++ test/__tests__/CourseContent.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import sinon from 'sinon';
+import SampleTestData from '../sampleTestData.js';
 
 import CourseContent from '../../client/src/components/CourseContent.jsx';
+import Section from '../../client/src/components/Section.jsx';
+import ContentHeader from '../../client/src/components/ContentHeader.jsx';
+// import { it } from 'date-fns/locale';
 
 describe('CourseComponent', () => {
 
@@ -20,7 +24,17 @@ describe('CourseComponent', () => {
 
   it('renders all immediate sub-components', () => {
     const wrapper = mount(<CourseContent />);
-    expect(wrapper.contains);
+    wrapper.setState({course: SampleTestData});
+    expect(wrapper.contains([Section, ContentHeader]));
+    wrapper.unmount();
+  });
+
+  it('renders correct number of sections', () => {
+    const wrapper = mount(<CourseContent />);
+    wrapper.setState({course: SampleTestData});
+    console.log(SampleTestData.sections.length);
+    console.log(wrapper.find(Section));
+    expect(wrapper.find(Section)).toHaveLength(SampleTestData.sections.length);
   });
 
 });
