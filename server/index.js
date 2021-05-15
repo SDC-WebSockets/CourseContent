155f1b200d8d9366083dfb2abb2a6b147e223025
kage1414 <kylejohnson92294@gmail.com>
2021-05-15 15:31:32 -0400

    Change static route

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 const controller = require('./controller.js');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const PORT = process.env.PORT || 9800;
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
-app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+app.use(cors());
+
+app.use('/', express.static(path.join(__dirname, '..', 'client', 'dist')));
+
+app.get('/bundle', (req, res) => {
+
+  const dir = fs.readdirSync(path.join(__dirname, '..', 'client', 'dist'));
+  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'course-content.js'));
+
+});
 
 app.get('/course/item', controller.course);
 
