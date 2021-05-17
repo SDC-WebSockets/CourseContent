13383a3b961b7382087f17566e958fde9e08ac03
kage1414 <kylejohnson92294@gmail.com>
2021-05-17 10:13:32 -0400

    Remove s3 webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackS3Plugin = require('webpack-s3-plugin');
+// const WebpackS3Plugin = require('webpack-s3-plugin');
 const config = require('./config.js');
 var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
