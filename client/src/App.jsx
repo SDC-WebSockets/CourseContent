b47660508f1db56a44f5a76e86ef8faa2fe1126c
kage1414 <kylejohnson92294@gmail.com>
2021-05-18 12:17:50 -0400

    Merge g	modified:   package-lock.json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import CourseContent from './components/CourseContent.jsx';
-<<<<<<< HEAD
 import axios from 'axios';
-=======
->>>>>>> 3a581d5119abd0a994a594a21c3311ac26db78b9
 import qs from 'qs';
 
 class App extends React.Component {
