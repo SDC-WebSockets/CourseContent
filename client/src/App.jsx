a79f2768c96d95dc26f4e57195bc78926b13740f
kage1414 <kylejohnson92294@gmail.com>
2021-05-20 21:28:25 -0400

    Console log location

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    console.log(location.pathname);
     const queries = qs.parse(window.location.search);
     const courseId = Number(queries['?courseId']);
     this.setState({courseId});
