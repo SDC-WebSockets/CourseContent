3911c857896564375b18d3a6c04b9fbafa0b7115
kage1414 <kylejohnson92294@gmail.com>
2021-05-25 22:16:43 -0400

    Fix port and app definition

--- server/start.js
+++ server/start.js
@@ -1,5 +1,6 @@
-const app = require('./server.js');
-const PORT = 3000;
+const server = require('./index.js');
+const app = server.app;
+const PORT = server.PORT;
 
 app.listen(PORT, () => {
   console.log('Listening on port:', PORT);
