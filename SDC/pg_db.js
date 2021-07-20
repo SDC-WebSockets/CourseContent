a8e056d65cc2d90a626614f14fe3e452691a6645
wapu-lab <nieruoxi@hotmail.com>
2021-07-19 22:12:56 -0500

    Finished refactoring the seeding pipeline and testing by seeding 10K courses.

--- SDC/pg_db.js
+++ SDC/pg_db.js
@@ -1,7 +1,7 @@
 require('dotenv').config(`${__dirname}/.env`);
 const { Sequelize, Model, DataTypes, QueryTypes } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.PGDB_DB_NAME, process.env.PGDB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 5432,
   dialect: 'postgres',
